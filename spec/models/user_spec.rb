53eb8f2f5359b19c2148266f577e3b59a9bd6408
Austin Dulay <dulayaustin@gmail.com>
2024-10-02 22:00:27 +0800

    Refactor user_spec model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    context "create existing user" do
+    context "uniqueness of email address" do
       let!(:existing_user) { FactoryBot.create(:user, email_address: "test@example.com") }
 
       it { should validate_uniqueness_of(:email_address).case_insensitive }
@@ -15,18 +15,20 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:last_name) }
     it { should accept_nested_attributes_for(:account) }
 
-    it "is valid when password and confirm password matches" do
-      user = User.new(password: "password", confirm_password: "password")
-      user.valid?
+    context "matching of password and confirm password" do
+      it "is valid when matched" do
+        user = User.new(password: "password", confirm_password: "password")
+        user.valid?
 
-      expect(user.errors.full_messages).to_not include("Password does not match")
-    end
+        expect(user.errors.full_messages).to_not include("Password does not match")
+      end
 
-    it "is invalid when password and confirm password does not match" do
-      user = User.new(password: "password", confirm_password: "incorrect_password")
-      user.valid?
+      it "is invalid when mismatched" do
+        user = User.new(password: "password", confirm_password: "incorrect_password")
+        user.valid?
 
-      expect(user.errors.full_messages).to include("Password does not match")
+        expect(user.errors.full_messages).to include("Password does not match")
+      end
     end
   end
 
