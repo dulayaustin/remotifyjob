f16f3614cb18f5cb645615a5499800895c22e656
Austin Dulay <dulayaustin@gmail.com>
2024-10-03 23:09:40 +0800

    Apply signs up system spec

---
+++ spec/system/sign_ups_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Sign ups", type: :system do
+  let!(:user) { FactoryBot.create(:user) }
+
+  it "is successfully authenticated" do
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email address", with: user.email_address
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+
+    expect(page).to have_css("#flash-container", text: "Signed in successfully.")
+    expect(current_path).to eq dashboard_path
+  end
+end
