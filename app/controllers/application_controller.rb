6ccd3a127c717eec2c94c5891b8a039c3cd8d360
Austin Dulay <dulayaustin@gmail.com>
2024-10-05 02:51:22 +0800

    Apply current_user and current_account methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def current_user
+    @current_user ||= Current.user
+  end
+  helper_method :current_user
+
+  def current_account
+    @current_account ||= current_user.account
+  end
+  helper_method :current_account
 end
