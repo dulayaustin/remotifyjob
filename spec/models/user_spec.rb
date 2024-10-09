f8ffb6969c2a9ee18890d38ebae7fc4cafa410f6
Austin Dulay <dulayaustin@gmail.com>
2024-10-10 01:18:14 +0800

    Update model user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "associations" do
+    it { should belong_to(:account) }
+    it { should have_many(:sessions).dependent(:destroy) }
+
+    it { should accept_nested_attributes_for(:account) }
+  end
+
   describe "validations" do
     context "uniqueness of email address" do
       let!(:existing_user) { FactoryBot.create(:user, email_address: "test@example.com") }
@@ -10,14 +17,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email_address) }
     it { should allow_value("user@example.com").for(:email_address) }
     it { should_not allow_value("user.example").for(:email_address).with_message(/invalid/) }
-    it { should validate_presence_of(:password) }
+
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
-    it { should accept_nested_attributes_for(:account) }
-  end
-
-  describe "associations" do
-    it { should belong_to(:account) }
-    it { should have_many(:sessions).dependent(:destroy) }
+    it { should validate_presence_of(:password) }
   end
 end
