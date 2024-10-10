bf06a299037532431e13af98f35b6b369b73c17c
Austin Dulay <dulayaustin@gmail.com>
2024-10-11 02:00:40 +0800

    Display resume using turbo frame

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
   resource :session, only: [ :new, :create, :destroy ]
   resources :passwords, param: :token
   resources :jobs
-  resources :applicants
+  resources :applicants do
+    get "resume", to: "resumes#show"
+  end
 end
